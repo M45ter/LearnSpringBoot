5868c236217f7b8d591c58aae432de4fa477d820
M45ter <569444304@qq.com>
2017-11-03 21:52:05 +0800

    Hello World

--- springbootdemo1/src/main/java/com/zey/springbootdemo1/Springbootdemo1Application.java
+++ springbootdemo1/src/main/java/com/zey/springbootdemo1/Springbootdemo1Application.java
@@ -2,9 +2,19 @@ package com.zey.springbootdemo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class Springbootdemo1Application {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Springbootdemo1Application.class, args);
